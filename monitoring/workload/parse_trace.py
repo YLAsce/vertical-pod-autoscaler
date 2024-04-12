09d2a4b01e1b81eb57da1ae9e3c350e191170134
MA Yuqiang <ylasce@gmail.com>
2024-04-12 17:55:44 +0200

    aa

--- monitoring/workload/parse_trace.py
+++ monitoring/workload/parse_trace.py
@@ -13,15 +13,15 @@ y_values = []
 
 for i in range(num_samples):
    if i % 2 < 1:
-       y_values.append(0.1)
+       y_values.append(0.05)
    else:
-       y_values.append(0.7)
+       y_values.append(0.75)
 
 timestamp = [1200 for _ in range(num_samples)]
 
 with open('trace.data', 'w') as f:
     for i in range(num_samples):
-        f.write('{} {} {}\n'.format(timestamp[i], 0.7, int(104857600 * (5*0.7))) ) # 50MB to 350MB
+        f.write('{} {} {}\n'.format(timestamp[i], y_values[i], int(104857600 * (5*y_values[i]))) ) # 25MB to 375MB
 
 
 
