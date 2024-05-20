1411c6ca8bf6623de63869f28b7c60c96483ab0e
Yuqiang Ma <yuqiangma@YuqiangtekiMacBook-Pro.local>
2024-05-21 00:15:05 +0200

    automatic commit from Company

--- pkg/recommender-simulator/draw_convex.py
+++ pkg/recommender-simulator/draw_convex.py
@@ -14,29 +14,29 @@ request_adjust = []
 with open('convex/param_sweep_result_{}.json'.format(dimension), 'r') as file:
     data = json.load(file)
     for d in data:
-        param.append(d[dimension])
-        average_gap.append(d['{}-average-gap'.format(observe)])
-        overrun_seconds.append(d['{}-overrun-seconds'.format(observe)])
-        request_adjust.append(d['{}-request-adjust-times'.format(observe)])
+        param.append(d['args'][dimension])
+        average_gap.append(d['result']['{}-average-gap'.format(observe)])
+        overrun_seconds.append(d['result']['{}-overrun-seconds'.format(observe)])
+        request_adjust.append(d['result']['{}-request-adjust-times'.format(observe)])
 
 fig, (ax1, ax2, ax3) = plt.subplots(3)
 
 ax1.plot(param, average_gap)
 ax1.set_title('{} Average Gap'.format(observe))
 ax1.set_xlabel(dimension + " value")
-ax1.set_ylabel('Resource')
+ax1.set_ylabel('mcores' if observe == 'cpu' else 'bytes')
 ax1.grid(True)
 
 ax2.plot(param, overrun_seconds)
 ax2.set_title('{} Overrun Seconds'.format(observe))
 ax2.set_xlabel(dimension + " value")
-ax2.set_ylabel('Resource')
+ax2.set_ylabel('seconds')
 ax2.grid(True)
 
 ax3.plot(param, request_adjust)
 ax3.set_title('{} Request Adjust Times'.format(observe))
 ax3.set_xlabel(dimension + " value")
-ax3.set_ylabel('Resource')
+ax3.set_ylabel('adjust times')
 ax3.grid(True)
 
 
