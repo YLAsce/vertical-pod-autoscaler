0852e170b530c8b7096d3380e3845467a39c1e4f
Yuqiang Ma <yuqiangma@YuqiangtekiMacBook-Pro.local>
2024-05-23 16:25:41 +0200

    automatic commit from Company

--- pkg/recommender-simulator/draw_convex.py
+++ pkg/recommender-simulator/draw_convex.py
@@ -3,7 +3,7 @@ import pprint
 import sys
 import matplotlib.pyplot as plt
 
-dimension = "ap-ml-memory-hyperparam-d"
+dimension = "ap-ml-memory-hyperparam-wu"
 small = 0.05
 mid = 0.01
 large = 0.01
@@ -17,28 +17,28 @@ request_adjust = []
 with open('convex/param_sweep_result_{}.json'.format(dimension), 'r') as file:
     data = json.load(file)
     for d in data:
-        if d['args'][dimension] > small:
-            continue
+        # if d['args'][dimension] > small:
+        #     continue
         param.append(d['args'][dimension])
         average_gap.append(d['result']['{}-average-gap'.format(observe)])
         overrun_seconds.append(d['result']['{}-overrun-seconds'.format(observe)])
         request_adjust.append(d['result']['{}-request-adjust-times'.format(observe)])
 
 fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(10, 7))
-
-ax1.plot(param, average_gap, marker='o', markersize=3)
+# , marker='o', markersize=3
+ax1.plot(param, average_gap)
 ax1.set_title('{} Average Gap'.format(observe))
 ax1.set_xlabel(dimension + " value")
 ax1.set_ylabel('mcores' if observe == 'cpu' else 'bytes')
 ax1.grid(True)
 
-ax2.plot(param, overrun_seconds, marker='o', markersize=3)
+ax2.plot(param, overrun_seconds)
 ax2.set_title('{} Overrun Seconds'.format(observe))
 ax2.set_xlabel(dimension + " value")
 ax2.set_ylabel('seconds')
 ax2.grid(True)
 
-ax3.plot(param, request_adjust, marker='o', markersize=3)
+ax3.plot(param, request_adjust)
 ax3.set_title('{} Request Adjust Times'.format(observe))
 ax3.set_xlabel(dimension + " value")
 ax3.set_ylabel('adjust times')
@@ -49,6 +49,6 @@ ax3.grid(True)
 plt.tight_layout()
 
 dname = dimension.split('-')[-1]
-fig.savefig('convex/convex_memory_{}_small{}.png'.format(dname, small))
+fig.savefig('convex/convex_memory_{}.png'.format(dname))
 # 显示图表
 plt.show()
